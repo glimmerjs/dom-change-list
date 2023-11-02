8061d0db89dee38ab67452ff8f050cc46a12aab0
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-11-02 11:44:53 -0400

    Move from the glimmer-vm repo

---
+++ lib/node-tokens.ts
@@ -0,0 +1,15 @@
+import type { NodeToken, NodeTokens, SimpleNode } from '@glimmer/interfaces';
+
+export class NodeTokensImpl implements NodeTokens {
+  private nodes: SimpleNode[] = [];
+
+  register(node: SimpleNode): NodeToken {
+    let token = this.nodes.length;
+    this.nodes.push(node);
+    return token;
+  }
+
+  reify(token: NodeToken): SimpleNode {
+    return this.nodes[token]!;
+  }
+}
